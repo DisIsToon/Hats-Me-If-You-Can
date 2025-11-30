3c9b98e88e7c872fa88c42b2e82001c2901de40f
DisIsToon <disistoon.skin@gmail.com>
2025-11-30 16:21:10 +0800

    Final inventory and brewing screen, removed Test SCene

--- Assets/Prefab/Script/Import/InventoryItem.cs
+++ Assets/Prefab/Script/Import/InventoryItem.cs
@@ -7,9 +7,9 @@ using TMPro;
 
 public class InventoryItem : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler
 {
-     // --- Is this item trashable --- //
+    // --- Is this item trashable --- //
     public bool isTrashable;
- 
+
     // --- Item Info UI --- //
     public GameObject itemInfoUI;
     public GameObject craftingItemInfoUI;
@@ -22,12 +22,19 @@ public class InventoryItem : MonoBehaviour, IPointerEnterHandler, IPointerExitHa
     public TextMeshProUGUI craftingItemInfoUI_itemDescription;
     public TextMeshProUGUI craftingItemInfoUI_itemFunctionality;
 
+    // --- New Icon Fields --- //
+    public Image itemInfoUI_itemIcon;
+    public Image craftingItemInfoUI_itemIcon;
+
+    // icon for this item
+    public Sprite iconForThisItem;
+
     public string thisName, thisDescription, thisFunctionality;
- 
+
     // --- Consumption --- //
     private GameObject itemPendingConsumption;
     public bool isConsumable;
- 
+
     public float healthEffect;
     public float hungerEffect;
 
@@ -35,11 +42,19 @@ public class InventoryItem : MonoBehaviour, IPointerEnterHandler, IPointerExitHa
     private GameObject itemPendingEquipping;
     public bool isNowInsideQuickSlot;
 
-    public bool isSelected;     
+    public bool isSelected;
 
     public bool isUseable;
- 
- 
+
+    public List<ItemIconData> itemIcons = new List<ItemIconData>();
+
+    [System.Serializable]
+    public class ItemIconData
+    {
+        public string itemName;
+        public Sprite icon;
+    }
+
     private void Start()
     {
         itemInfoUI = InventorySystem.Instance.ItemInfoUI;
@@ -52,70 +67,87 @@ public class InventoryItem : MonoBehaviour, IPointerEnterHandler, IPointerExitHa
         craftingItemInfoUI_itemName = craftingItemInfoUI.transform.Find("itemName").GetComponent<TextMeshProUGUI>();
         craftingItemInfoUI_itemDescription = craftingItemInfoUI.transform.Find("itemDescription").GetComponent<TextMeshProUGUI>();
         craftingItemInfoUI_itemFunctionality = craftingItemInfoUI.transform.Find("itemFunctionality").GetComponent<TextMeshProUGUI>();
+
+        // --- Find the icon components --- //
+        itemInfoUI_itemIcon = itemInfoUI.transform.Find("itemIcon").GetComponent<Image>();
+        craftingItemInfoUI_itemIcon = craftingItemInfoUI.transform.Find("itemIcon").GetComponent<Image>();
     }
 
     void Update()
     {
-        if(isSelected)
-        {
+        if (isSelected)
             gameObject.GetComponent<DragDrop>().enabled = false;
-        }
         else
-        {
             gameObject.GetComponent<DragDrop>().enabled = true;
-        }
     }
- 
-    // Triggered when the mouse enters into the area of the item that has this script.
+
+    private Sprite LoadIcon(string itemName)
+    {
+        return Resources.Load<Sprite>("ItemIcons/" + itemName);
+    }
+
+
+    private Sprite GetIconByName(string name)
+    {
+        foreach (var data in itemIcons)
+        {
+            if (data.itemName == name)
+                return data.icon;
+        }
+
+        return null; // if no match
+    }
+
     public void OnPointerEnter(PointerEventData eventData)
     {
-        if(CraftingSystem.Instance.isOpen)
+        if (CraftingSystem.Instance.isOpen)
         {
             craftingItemInfoUI.SetActive(true);
-            craftingItemInfoUI.text = thisName;
-            craftingItemInfoUI.text = thisDescription;
-            craftingItemInfoUI.text = thisFunctionality;
+
+            craftingItemInfoUI_itemName.text = thisName;
+            craftingItemInfoUI_itemDescription.text = thisDescription;
+            craftingItemInfoUI_itemFunctionality.text = thisFunctionality;
+
+            craftingItemInfoUI_itemIcon.sprite = LoadIcon(thisName);
+
         }
         else
         {
             itemInfoUI.SetActive(true);
+
             itemInfoUI_itemName.text = thisName;
             itemInfoUI_itemDescription.text = thisDescription;
             itemInfoUI_itemFunctionality.text = thisFunctionality;
+
+            itemInfoUI_itemIcon.sprite = LoadIcon(thisName);
         }
-
-
-        
     }
- 
-    // Triggered when the mouse exits the area of the item that has this script.
+
+
     public void OnPointerExit(PointerEventData eventData)
     {
         itemInfoUI.SetActive(false);
         craftingItemInfoUI.SetActive(false);
     }
- 
-    // Triggered when the mouse is clicked over the item that has this script.
+
     public void OnPointerDown(PointerEventData eventData)
     {
-        //Right Mouse Button Click on
         if (eventData.button == PointerEventData.InputButton.Right)
         {
             if (isConsumable)
             {
                 SoundManager.Instance.PlaySound(SoundManager.Instance.eatingSound);
-                // Setting this specific gameobject to be the item we want to destroy later
                 itemPendingConsumption = gameObject;
             }
-            if(isEquippable && isNowInsideQuickSlot == false && EquipSystem.Instance.CheckIfFull() == false)
+
+            if (isEquippable && isNowInsideQuickSlot == false && EquipSystem.Instance.CheckIfFull() == false)
             {
                 EquipSystem.Instance.AddToQuickSlots(gameObject);
                 isNowInsideQuickSlot = true;
             }
         }
     }
- 
-    // Triggered when the mouse button is released over the item that has this script.
+
     public void OnPointerUp(PointerEventData eventData)
     {
         if (eventData.button == PointerEventData.InputButton.Right)
@@ -123,9 +155,8 @@ public class InventoryItem : MonoBehaviour, IPointerEnterHandler, IPointerExitHa
             if (isConsumable && itemPendingConsumption == gameObject)
             {
                 DestroyImmediate(gameObject);
-                InventorySystem.Instance.ReCalculateList(); 
+                InventorySystem.Instance.ReCalculateList();
             }
-
         }
     }
 
@@ -134,17 +165,13 @@ public class InventoryItem : MonoBehaviour, IPointerEnterHandler, IPointerExitHa
         itemInfoUI.SetActive(false);
         craftingItemInfoUI.SetActive(false);
 
-        InventorySystem.Instance.isOpen = false; 
+        InventorySystem.Instance.isOpen = false;
         InventorySystem.Instance.inventoryScreenUI.SetActive(false);
 
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
 
         SelectionManager.Instance.EnableSelection();
-        SelectionManager.Instance.enabled = true; 
-
-       
+        SelectionManager.Instance.enabled = true;
     }
- 
- 
 }
