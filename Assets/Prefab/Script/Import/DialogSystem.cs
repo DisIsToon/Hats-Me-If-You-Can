ce69144b4e18365f2c0452968f484dafc1af7603
DisIsToon <disistoon.skin@gmail.com>
2026-03-29 18:34:04 +0800

    npc tweaks

--- Assets/Prefab/Script/Import/DialogSystem.cs
+++ Assets/Prefab/Script/Import/DialogSystem.cs
@@ -28,6 +28,9 @@ public class DialogSystem : MonoBehaviour
     public GameObject liraImage;
     public GameObject mallowImage;
     public GameObject tulipImage;
+    public GameObject ivyImage;
+    public GameObject chaseImage;
+    public GameObject louImage;
 
     private void Awake()
     {
@@ -63,6 +66,10 @@ public class DialogSystem : MonoBehaviour
         mallowImage.SetActive(npcName == "Mallow");
         tulipImage.SetActive(npcName == "Tulip");
 
+        ivyImage.SetActive(npcName == "Ivy");
+        chaseImage.SetActive(npcName == "Chase");
+        louImage.SetActive(npcName == "Lou");
+
         speakerNameText.text = npcName;
     }
 
@@ -78,6 +85,9 @@ public class DialogSystem : MonoBehaviour
         liraImage.SetActive(false);
         mallowImage.SetActive(false);
         tulipImage.SetActive(false);
+        ivyImage.SetActive(false);
+        chaseImage.SetActive(false);
+        louImage.SetActive(false);
     }
 
     public void OpenDialogUI()
